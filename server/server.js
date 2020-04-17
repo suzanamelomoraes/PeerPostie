d42b635d06ce760ed5a0484d77b17e227bc42b24
Andria-Hibe <andriacohibe@gmail.com>
2020-04-18 01:50:04 +1200

    Setup CORS

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+server.use(cors())
 
 module.exports = server
