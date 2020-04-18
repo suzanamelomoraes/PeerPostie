1f0ee98fb385de39c64c77cf931a4ba53d3fa234
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-18 14:40:38 +1200

    create delivery table database and define columns for the delivey details

--- migrations/20200418141740_delivery.js
+++ migrations/20200418141740_delivery.js
@@ -1,8 +1,17 @@
+exports.up = knex =>
+  knex.schema.createTable('delivery', (table) => {
+    table.increments('id').primary()
+    table.string('pick_up')
+    table.string('deliver_to')
+    table.string('expected_time')
+    table.string('package_type')
+      .references('package_type.id')
+    table.string('request_maker')
+      .references('users.id')
+    table.integer('delivery_person')
+      .references('users.id')
+      table.integer('prices')
+      .references('prices.id')
+  })
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = knex => knex.schema.dropTable('delivery')
