c74109bbd87bb23031323f270d4c62bb8066b012
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-18 14:25:20 +1200

    create users table database and define columns

--- migrations/20200418141705_users.js
+++ migrations/20200418141705_users.js
@@ -1,8 +1,18 @@
 
-exports.up = function(knex) {
-  
-};
+exports.up = knex =>
+knex.schema.createTable('users', (table) => {
+  table.increments('id').primary()
+  table.string('name')
+  table.string('address')
+  table.integer('phone')
+  table.string('email')
+  table.integer('payment_details')
+  table.integer('rating')
+  table.string('password')
+  table.integer('latitude')
+  table.integer('longitude')
+  table.string('status')
+    .references('status.id')
+})
 
-exports.down = function(knex) {
-  
-};
+exports.down = knex => knex.schema.dropTable('users')
