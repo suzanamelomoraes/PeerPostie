72add723b81d6a98201873a68fb1810f8de30e48
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-18 15:23:11 +1200

    feed status seed with data

--- seeds/status.js
+++ seeds/status.js
@@ -1,13 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('status').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('status').insert([
+        {id: 1, colName: 'requesters'},
+        {id: 2, colName: 'deliverers'}
       ]);
     });
 };
