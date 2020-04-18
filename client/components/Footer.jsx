eab96450c95534a705f767f1bfd82ef2411d781b
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-18 22:50:48 +1200

    update columns sizes and classes, update text colous and align items of the footer, replace icons images by icons from bootstrap, include links to be used when the page is ready

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -2,52 +2,57 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class='footer bg-transparent'>
-      <div class='container'>
+    <footer>
+      <div className='container'>
         <div className='row'>
-          <div className='col md-6 flex-column' >
-            <img
-              src='-peer-postie-logo-.png'
-              className='img-fluid img-footer'
-              alt='Peer Postie logo'
-            />
-            <ul class='list-inline'>
-                <li className='list-inline-item'><img
-              src='facebook.png'
-              className='img-fluid logo'
-              alt='Facebook logo'
-            /></li>
-                <li className='list-inline-item'><img
-              src='linkedin.png'
-              className='img-fluid logo'
-              alt='Linkedin logo'
-            /></li>
-                <li className='list-inline-item'><img
-              src='instagram.png'
-              className='img-fluid logo'
-              alt='Instagram logo'
-            /></li>
+          <div className='col md-6'>
+            <a href='#'>
+              <img
+                src='-peer-postie-icon.png'
+                className='img-fluid img-footer'
+                alt='Peer Postie logo'
+              />
+            </a>
+
+            <ul className='list-inline'>
+              <li className='list-inline-item'>
+                <a href='#facebook' class='fa fa-facebook'></a>
+              </li>
+              <li className='list-inline-item'>
+                <a href='#twitter' class='fa fa-twitter'></a>
+              </li>
+              <li className='list-inline-item'>
+                <a href='#instagram' class='fa fa-instagram'></a>
+              </li>
+              <li className='list-inline-item'>
+                <a href='#linkedin' class='fa fa-linkedin'></a>
+              </li>
             </ul>
           </div>
 
-          <div className='col md-6'>
-            <ul class='list-inline'>
-              <li>
-                <span class='text-muted list-inline-item small'>About us</span>
-              </li>
-              <li>
-                <span class='text-muted list-inline-item small'>Contact Us</span>
-              </li>
-              <li>
-                <span class='text-muted list-inline-item small'>Terms & Conditions</span>
-              </li>
-              <li>
-                <span class='text-muted list-inline-item small'>Privacy Policy</span>
-              </li>
-            </ul>
-            <div className='col md-12'>
-            <span class='primary list-inline-item small'>© Copyright PeerPostie 2020</span>
-            </div>
+          <div className='navbar col md-6'>
+            <a href='#about'>
+              <span className='text-white small'>About us</span>
+            </a>
+
+            <a href='#contact'>
+              <span className='text-white small'>Contact Us</span>
+            </a>
+
+            <a href='#term'>
+              <span className='text-white small'>Terms & Conditions</span>
+            </a>
+
+            <a href='#policy'>
+              <span className='text-white small'>Privacy Policy</span>
+            </a>
+          </div>
+        </div>
+        <div className='row text-center'>
+          <div className='col md-12'>
+            <span className='list-inline-item small text-white'>
+              © Copyright PeerPostie 2020
+            </span>
           </div>
         </div>
       </div>
