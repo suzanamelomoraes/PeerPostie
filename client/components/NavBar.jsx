43c35a4fcf68f30fe921feb2013e89092d310f7a
Ashleigh Ah Ta <ashleigh.ahta@gmail.com>
2020-04-18 21:40:24 +1200

    add logo image, import Nav

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,26 +1,25 @@
-import React, { useState } from "react";
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import { Nav } from "react-bootstrap";
 
-class NavBar extends React.Component {
+export default function NavBar() {
 
-
-  render() {
-      return (
-        <>
-          <Container>
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">PostPeeps</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link noValidate href="#join">Join Us</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-            </Navbar>
-          </Container>
-        </>
-      )
-    }
-}
-export default  connect(Nav)
+    return (
+        <Navbar sticky="top" bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              src="peer-postie-icon.png"
+              // width="30"
+              // height="30"
+              //className="d-inline-block align-top"
+              alt="PeerPostie logo"
+            ></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav inline="right" className="mr-auto">
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#join">Join Us</Nav.Link>
+            </Nav>
+        </Navbar>
+    )
+  }
