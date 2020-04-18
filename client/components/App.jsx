bbce902e3af6e12b63c20887b6f0c5573dfc5e4d
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-18 20:49:28 +1200

    removed Footer component from App to MainSection

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Footer from "./Footer";
 import MainSection from "./MainSection";
 
 const App = () => {
   return (
     <>
       <MainSection />
-      <Footer />
     </>
   );
 };
