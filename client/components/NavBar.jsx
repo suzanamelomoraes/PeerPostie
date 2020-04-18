96aa49c1b7b0802be3fc496e6b53f57db3b77eee
Ashleigh Ah Ta <ashleigh.ahta@gmail.com>
2020-04-18 21:57:27 +1200

    aligned links within navbar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -3,23 +3,24 @@ import Navbar from "react-bootstrap/Navbar";
 import { Nav } from "react-bootstrap";
 
 export default function NavBar() {
-
     return (
-        <Navbar sticky="top" bg="dark" variant="dark">
+        <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
           <Navbar.Brand href="#home">
             <img
               src="peer-postie-icon.png"
-              // width="30"
-              // height="30"
-              //className="d-inline-block align-top"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
               alt="PeerPostie logo"
-            ></img>
+          />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Nav inline="right" className="mr-auto">
+        <Navbar.Toggle />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav inline="true">
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#join">Join Us</Nav.Link>
-            </Nav>
+          </Nav>
+        </Navbar.Collapse>
         </Navbar>
     )
   }
