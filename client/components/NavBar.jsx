98e7ba48e00ca8bc02feda0a991e6cf2c2ef8488
JillGlass <glass.nz@gmail.com>
2020-04-18 23:22:43 +1200

    im in the wrong branch, have updated navbar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import { Nav } from "react-bootstrap";
+import { Nav, Row, Col } from "react-bootstrap";
 
 export default function NavBar() {
     return (
-        <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
+        <Navbar className='navbar' sticky="top"  expand="lg">
+          <Col lg={6}>
           <Navbar.Brand href="#home">
             <img
-              src="peer-postie-icon.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
+              src="-peer-postie-icon.png"
+              className="d-inline-block align-top logo-nav"
               alt="PeerPostie logo"
           />
           </Navbar.Brand>
+          </Col>
+          
+          <Navbar.Brand  className='logo-title' href="#home">PeerPostie</Navbar.Brand>
+          
+          <Col className='nav-links' lg={6}>
         <Navbar.Toggle />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Navbar.Collapse id="basic-navbar-nav" >
             <Nav inline="true">
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#join">Join Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        </Col>
         </Navbar>
     )
   }
