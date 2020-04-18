1e2863a646d0e8971e060b853f4265f09d3ab8d1
Andria-Hibe <andriacohibe@gmail.com>
2020-04-18 18:51:41 +1200

    Update tables and columns

--- server/db/migrations/20200418141705_users.js
+++ server/db/migrations/20200418141705_users.js
@@ -2,17 +2,14 @@
 exports.up = knex =>
 knex.schema.createTable('users', (table) => {
   table.increments('id').primary()
-  table.string('name')
+  table.string('first_name')
+  table.string('last_name')
   table.string('address')
   table.integer('phone')
   table.string('email')
-  table.integer('payment_details')
   table.integer('rating')
-  table.string('password')
   table.integer('latitude')
   table.integer('longitude')
-  table.string('status')
-    .references('status.id')
 })
 
 exports.down = knex => knex.schema.dropTable('users')
--- server/db/migrations/20200418141731_status.js
+++
@@ -1,8 +0,0 @@
-exports.up = knex =>
-  knex.schema.createTable('status', (table) => {
-    table.increments('id').primary()
-    table.string('requester')
-    table.string('deliverer')
-  })
-
-exports.down = knex => knex.schema.dropTable('status')
