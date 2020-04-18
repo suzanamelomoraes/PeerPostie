7395cee7da15ad6a6bee29ad34edaadec869de8a
JeyaRaniga <jayafj@gmail.com>
2020-04-19 00:21:32 +1200

    added api fn to add delivery

--- client/apis/addDelivery.js
+++ client/apis/addDelivery.js
@@ -1,8 +1,13 @@
 import request from 'superagent'
 
-const apiURL = 'htts/'
+const apiURL = 'http://localhost:3000/api/delivery'
 
-export function addDelivery(deliver){
+export function addDelivery(delivery){
     return request
-    .post()
+    .post(apiURL)
+    .send(delivery)
+    .then(res => res.body)
+    .catch(err =>{
+      throw new Error(err.message) 
+    })
 }
