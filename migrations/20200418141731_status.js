b5f0f4b5ba008fc52aa2885d04b972d39f6f184f
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-18 14:28:25 +1200

    create status table database and define columns for requesters and deliveres status

--- migrations/20200418141731_status.js
+++ migrations/20200418141731_status.js
@@ -1,8 +1,8 @@
+exports.up = knex =>
+  knex.schema.createTable('status', (table) => {
+    table.increments('id').primary()
+    table.string('requester')
+    table.string('deliverer')
+  })
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = knex => knex.schema.dropTable('status')
